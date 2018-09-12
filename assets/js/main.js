2aa5cddd3b0201716ae2b16647a6c58b01bc9a72
Joe Stokes <joestokes1994@gmail.com>
2018-09-12 12:44:29 +0000

    fixed duplication bug, cleaned up code and modified the way each function is called. Also added tooltip for the track data

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,8 +31,9 @@ if (!_token) {
 
 
 // Search bar feature
+// Function is called on button click
 
-function searchSong(name) {
+function searchSong() {
     $("#find-track").click(function(e) {
         let searchResults = document.getElementById('search-results-body');
         searchResults.innerHTML = "";
@@ -43,58 +44,40 @@ function searchSong(name) {
             beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + _token); },
             success: function(data) {
                 // console.log(data);  TEST
-                // Need to add feedback to user if there is no results found
 
                 // Append the returned data to a scrolling table for user selection
                 // As returned JSON is an array need .map()
                 data.tracks.items.map(function(track, artists, album) {
-                    let item = $('<tr>' + '<td>' + track.name + '</td>' + '<td>' + track.artists[0].name + "</td>" + '<td>' + track.album.name + '</td>' +
-                        '<td id="hidden-table" class="hidden-track">' + track.id + '</td>' + '<td id="hidden-table" class="hidden-artist">' + track.artists[0].id + '</td>' +
-                        '<td id="hidden-table" class="hidden-album">' + track.album.id + '</td>' + '</tr>');
+                    let item = $('<tr> <td>' + track.name + '</td> <td>' + track.artists[0].name + '</td> <td>' + track.album.name +
+                        '</td> <td class="hidden-track">' + track.id + '</td> <td class="hidden-artist">' + track.artists[0].id + '</td> <td class="hidden-album">' + track.album.id + '</td> </tr>');
                     item.appendTo($('#search-results-body'));
 
-                    //Invoke the ID finder function only when this information is apended to tbody
-                    findId();
+                    $("tr").click(function() {
+
+                        // Empty containers on click
+                        // Track Info
+                        let trackListInfo = document.getElementById("track-list");
+                        trackListInfo.innerHTML = "";
+                        // Artist info
+                        let artistListInfo = document.getElementById("artist-info");
+                        artistListInfo.innerHTML = "";
+                        // Album info
+                        let albumListInfo = document.getElementById("album-info");
+                        albumListInfo.innerHTML = "";
+                        // Track data info
+                        let trackDataInfo1 = document.getElementById("track-data-1");
+                        trackDataInfo1.innerHTML = "";
+                        let trackDataInfo2 = document.getElementById("track-data-2");
+                        trackDataInfo2.innerHTML = "";
+                        let dataDesc = document.getElementById("data-track-hover");
+                        dataDesc.innerHTML = "";
+
+                        // Invoke each function passing in the relevant ids for the GET calls
+                        $(this)(trackInfo(track.id), artistInfo(track.artists[0].id), albumInfo(track.album.id), trackData(track.id));
+                    });
                 });
             }
         });
-    });
-}
-
-
-// When a row is clicked, the track id of that row is stored as a variable and passed to the functions for each container
-// Also the containers holding track info is emptied on row click.
-function findId() {
-    $("tr").click(function() {
-        // Track info
-        let trackListInfo = document.getElementById("track-list");
-        trackListInfo.innerHTML = "";
-        // Artist info
-        let artistListInfo = document.getElementById("artist-info");
-        artistListInfo.innerHTML = "";
-        // Album info
-        let albumListInfo = document.getElementById("album-info");
-        albumListInfo.innerHTML = "";
-        // Track data info
-        let trackDataInfo1 = document.getElementById("track-data-1");
-        trackDataInfo1.innerHTML = "";
-        let trackDataInfo2 = document.getElementById("track-data-2");
-        trackDataInfo2.innerHTML = "";
-
-        //Get the IDs for the URL search from the non visible part of the table
-
-        var $songId = $(this).find(".hidden-track").text();
-        //console.log($songId); //TEST
-        var $artistId = $(this).find(".hidden-artist").text();
-        //console.log($artistId); //TEST
-        var $albumId = $(this).find(".hidden-album").text();
-
-        //Invoke the info finder functions, passing in the ID for each container
-
-        trackInfo($songId);
-        artistInfo($artistId);
-        albumInfo($albumId);
-        trackData($songId);
     });
 }
 
@@ -107,9 +90,9 @@ function trackInfo(id) {
         type: "GET",
         beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + _token); },
         success: function(data) {
-            let item = $('<ul class="list-style section-text">' + '<li>' + "<span class='list-heading'>Track Name:</span> " + data.name + '</li>' + '<li>' +
-                "<span class='list-heading'>Length (ms):</span> " + data.duration_ms + '</li>' + '<li>' + "<span class='list-heading'>Popularity:</span> " +
-                data.popularity + '</li>' + '<li>' + "<span class='list-heading'>Explicit:</span> " + data.explicit + '</li>' + '</ul>');
+            let item = $('<ul class="list-style section-text"> <li> <span class="list-heading">Track Name: </span>' + data.name +
+                '</li> <li> <span class="list-heading">Length (ms): </span>' + data.duration_ms + '</li> <li>' + '<span class="list-heading">Popularity: </span>' +
+                data.popularity + '</li> <li>' + '<span class="list-heading">Explicit: </span>' + data.explicit + '</li> </ul>');
             item.appendTo($('#track-list'));
         }
     });
@@ -123,9 +106,9 @@ function artistInfo(id) {
         type: "GET",
         beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + _token); },
         success: function(data) {
-            let item = $('<ul class="list-style section-text">' + '<li>' + "<span class='list-heading'>Artist Name:</span> " + data.name + '</li>' + '<li>' +
-                "<span class='list-heading'>Followers:</span> " + data.followers.total + '</li>' + '<li>' + "<span class='list-heading'>Genres:</span> " +
-                data.genres + '</li>' + '<li>' + "<span class='list-heading'>Popularity:</span> " + data.popularity + '</li>' + '</ul>');
+            let item = $('<ul class="list-style section-text"> <li>' + '<span class="list-heading">Artist Name: </span>' + data.name + '</li> <li>' +
+                '<span class="list-heading">Followers: </span>' + data.followers.total + '</li> <li> <span class="list-heading">Genres: </span>' +
+                data.genres + '</li> <li>' + '<span class="list-heading">Popularity: </span>' + data.popularity + '</li> </ul>');
             item.appendTo($('#artist-info'));
         }
     });
@@ -139,14 +122,13 @@ function albumInfo(id) {
         type: "GET",
         beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + _token); },
         success: function(data) {
-            let item = $('<ul class="list-style section-text">' + '<li>' + "<span class='list-heading'>Album Name:</span> " + data.name + '</li>' + '<li>' +
-                "<span class='list-heading'>Release Date:</span> " + data.release_date + '</li>' + '<li>' + "<span class='list-heading'>Total Tracks:</span>" +
-                data.total_tracks + '</li>' + '<li>' + "<span class='list-heading'>Popularity:</span> " + data.popularity + '</li>' + '</ul>');
+            let item = $('<ul class="list-style section-text"> <li>' + '<span class="list-heading">Album Name:</span> ' + data.name + '</li> <li>' +
+                '<span class="list-heading">Release Date:</span> ' + data.release_date + '</li> <li>' + '<span class="list-heading">Total Tracks:</span> ' +
+                data.total_tracks + '</li> <li>' + '<span class="list-heading">Popularity:</span> ' + data.popularity + '</li> </ul>');
             item.appendTo($('#album-info'));
         }
     });
 }
-
 
 // Collect the track ID for the selected song and insert it in the URL. Append the selected results to the container.
 function trackData(id) {
@@ -156,18 +138,26 @@ function trackData(id) {
         type: "GET",
         beforeSend: function(xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + _token); },
         success: function(data) {
-            let item1 = $('<ul class="list-style section-text">' + '<li>' + "<span class='list-heading'>Acousticness:</span> " + data.acousticness + '</li>' + '<li>' +
-                "<span class='list-heading'>Danceability:</span> " + data.danceability + '</li>' + '<li>' + "<span class='list-heading'>Energy:</span> " + data.energy +
-                '</li>' + '<li>' + "<span class='list-heading'>Key:</span> " + data.key + '</li>' + '<li>' + "<span class='list-heading'>Instrumentalness:</span> " +
+            let item1 = $('<ul class="list-style section-text"> <li title="How acoustic the track is (0.0-1.0)">' + '<span class="list-heading">Acousticness:</span> ' + data.acousticness +
+                '</li> <li title="How suitable a track is to dance too based on a combination of elements (0.0-1.0)">' + '<span class="list-heading">Danceability:</span> ' + data.danceability +
+                '</li> <li title="A perceptual measure of intensity and activity">' + '<span class="list-heading">Energy:</span> ' + data.energy + '</li> <li title="The key that the track is in">' +
+                '<span class="list-heading">Key:</span> ' + data.key + '</li> <li title="Predicts how many vocals a track has (0.0-1.0)">' + '<span class="list-heading">Instrumentalness:</span> ' +
                 data.instrumentalness + '</li>' + '</ul>');
             item1.appendTo($('#track-data-1'));
-            let item2 = $('<ul class="list-style section-text">' + '<li>' + "<span class='list-heading'>Liveness:</span> " + data.liveness + '</li>' + '<li>' +
-                "<span class='list-heading'>Loudness:</span> " + data.loudness + '</li>' + '<li>' + "<span class='list-heading'>Speechiness:</span> " + data.speechiness +
-                '</li>' + '<li>' + "<span class='list-heading'>Tempo:</span> " + data.tempo + '</li>' + '<li>' + "<span class='list-heading'>Valence</span> : " + data.valence +
-                '</li>' + '</ul>');
+            let item2 = $('<ul class="list-style section-text"> <li title="Detects presence of an audience. Probabability the track was performed live (0.0-1.0)">' + '<span class="list-heading">Liveness:</span> ' +
+                data.liveness + '</li> <li title="Overall Loudness of track in dB (-60-0dB)">' + '<span class="list-heading">Loudness:</span> ' + data.loudness +
+                '</li> <li title="Detects presence of spoken word on the track (0.0-1.0)">' + '<span class="list-heading">Speechiness:</span> ' + data.speechiness + '</li> <li title="Overall tempo of the track in BPM">' +
+                '<span class="list-heading">Tempo:</span> ' + data.tempo + '</li> <li title="Muscial positiveness conveyed from the track (0.0-1.0)">' + '<span class="list-heading">Valence</span> : ' + data.valence +
+                '</li> </ul>');
             item2.appendTo($('#track-data-2'));
+            let item3 = $('<p class="section-text data-desc">Hover over each feature to find out what they mean</p>');
+            item3.appendTo($('#data-track-hover'));
+
+            // Tooltip function using jQuery UI to give details to user about what each feature means
+            $(function() {
+                $(document).tooltip();
+            });
+
         }
     });
 }
-
-
